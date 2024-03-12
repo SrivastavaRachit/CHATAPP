67b0bb8c934a00cfcbe9532912ef2d8198965bbb
SrivastavaRachit <rachitsrivastava@gmail.com>
2024-03-12 16:24:10 +0530

    nine

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,12 +1,73 @@
 import React from 'react'
 import './myStyles.css'
+import logo from '../icons/live-chat_512px.png';
+import { IconButton } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import { useSelector } from 'react-redux';
+import { AnimatePresence, motion } from "framer-motion";
 
-const Users = () => {
+const Groups = () => {
+  const lightTheme = useSelector((state) => state.themeKey);
+
   return (
-    <div>
-        U
-    </div>
+    <AnimatePresence>
+    <motion.div
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 0 }}
+      transition={{
+        ease: "anticipate",
+        duration: "0.3",
+      }}
+      className='list-container'>
+      <div className={'ug-header' + (lightTheme ? "" : " dark")}>
+        <img src={logo} alt='logo' style={{
+          height: "2rem", width: "2rem", marginLeft: "10px"
+        }} />
+
+        <p className='ug-title'>Online Users</p>
+      </div>
+
+      <div className='sb-search'>
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+        <input placeholder='Search' className='search-box' />
+      </div>
+
+      <div className='ug-list'>
+        <motion.div whileHover={{ scale: 1.01 }}
+          whileTap={{ scale: 0.98 }}
+          className='list-tem'>
+          <p className='con-icon'>T</p>
+          <p className='con-title'>Test User</p>
+        </motion.div>
+
+        <motion.div whileHover={{ scale: 1.01 }}
+          whileTap={{ scale: 0.98 }}
+          className='list-tem'>
+          <p className='con-icon'>T</p>
+          <p className='con-title'>Test User</p>
+        </motion.div>
+
+        <motion.div whileHover={{ scale: 1.01 }}
+          whileTap={{ scale: 0.98 }}
+          className='list-tem'>
+          <p className='con-icon'>T</p>
+          <p className='con-title'>Test User</p>
+        </motion.div>
+
+        <motion.div whileHover={{ scale: 1.01 }}
+          whileTap={{ scale: 0.98 }}
+          className='list-tem'>
+          <p className='con-icon'>T</p>
+          <p className='con-title'>Test User</p>
+        </motion.div>
+      </div>
+
+    </motion.div>
+  </AnimatePresence>
   )
 }
 
-export default Users;
+export default Groups;
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,14 +2,20 @@ import React from 'react'
 import './myStyles.css'
 import logo from '../icons/live-chat (1).png'
 import { useSelector } from 'react-redux';
+import { motion } from "framer-motion";
 
 const Welcome = () => {
   const lightTheme = useSelector((state) => state.themeKey);
 
   return (
     <div className={"welcome-container" + (lightTheme ? "" : " dark")}>
-      <img src={logo} alt='logo'
-        className='welcome-logo' />
+      <motion.img
+        drag
+        whileTap={{ scale: 1.05, rotate: 360 }}
+        src={logo}
+        alt="Logo"
+        className="welcome-logo"
+      />
       <p>View and text directly to people present
         in the chat Rooms.</p>
     </div>
